b733c7ef51efd2aec5162cc6d2f26d6c22d07a8b
Itztarunk51 <103349024+Itztarunk51@users.noreply.github.com>
2023-05-28 13:02:23 +0530

    Update calc.sol

--- calc.sol
+++ calc.sol
@@ -2,103 +2,126 @@
 pragma solidity ^0.8.0;
 
 contract StringInputCalculator {
-    uint256 public computedValue;
-
-    function computeStringExpression(string memory stringMathExpression) public {
-        computedValue = evaluateExpression(stringMathExpression);
-    }
-
-    function evaluateExpression(string memory expression) private pure returns (uint256) {
-        bytes memory expr = bytes(expression);
-        uint256 result;
-        uint256 numBuffer;
-        uint256 sign = 1;
-        bool hasNumBuffer;
-
-        for (uint256 i = 0; i < expr.length; i++) {
-            uint8 byteValue = uint8(expr[i]);
-
-            if (byteValue >= 48 && byteValue <= 57) {
-                numBuffer = numBuffer * 10 + (byteValue - 48);
-                hasNumBuffer = true;
-            } else if (byteValue == 43 || byteValue == 45 || byteValue == 42 || byteValue == 47) {
-                if (hasNumBuffer) {
-                    result = performOperation(result, numBuffer, sign);
-                    numBuffer = 0;
-                    hasNumBuffer = false;
-                }
-
-                if (byteValue == 43) {
-                    sign = 1; // Addition
-                } else if (byteValue == 45) {
-                    sign = 2; // Subtraction
-                } else if (byteValue == 42) {
-                    sign = 3; // Multiplication
-                } else if (byteValue == 47) {
-                    sign = 4; // Division
-                }
-            } else if (byteValue == 40) {
-                uint256 subExpressionStartIndex = i + 1;
-                uint256 subExpressionEndIndex = findClosingParenthesis(expr, subExpressionStartIndex);
-                string memory subExpression = substring(expr, subExpressionStartIndex, subExpressionEndIndex);
-                numBuffer = evaluateExpression(subExpression);
-                hasNumBuffer = true;
-                i = subExpressionEndIndex;
+    int256 private contractResult; // Renamed private variable
+    
+    function computeString(string memory expression) public {
+        string[] memory rpn = toRPN(expression);
+        int256[] memory stack = new int256[](rpn.length);
+        uint256 top = 0;
+        
+        for (uint256 i = 0; i < rpn.length; i++) {
+            if (isNumber(rpn[i])) {
+                stack[top++] = parseInt(rpn[i]);
+            } else {
+                int256 b = stack[--top];
+                int256 a = stack[--top];
+                stack[top++] = applyOperator(a, bytes(rpn[i])[0], b);
             }
         }
-
-        if (hasNumBuffer) {
-            result = performOperation(result, numBuffer, sign);
+        
+        contractResult = stack[0];
+    }
+    
+    function getResult() public view returns (int256) {
+        return contractResult;
+    }
+    
+    function toRPN(string memory expression) internal pure returns (string[] memory) {
+        bytes memory b = bytes(expression);
+        string[] memory output = new string[](b.length);
+        uint256 outputLength = 0;
+        bytes1[] memory operatorStack = new bytes1[](b.length);
+        uint256 operatorStackLength = 0;
+        
+        for (uint256 i = 0; i < b.length; i++) {
+            if (b[i] >= "0" && b[i] <= "9") {
+                uint256 start = i;
+                while (i < b.length && b[i] >= "0" && b[i] <= "9") {
+                    i++;
+                }
+                output[outputLength++] = substring(expression, start, i);
+                i--;
+            } else if (b[i] == "+" || b[i] == "-" || b[i] == "*" || b[i] == "/") {
+                while (operatorStackLength > 0 && precedence(operatorStack[operatorStackLength - 1]) >= precedence(b[i])) {
+                    output[outputLength++] = string(abi.encodePacked(operatorStack[--operatorStackLength]));
+                }
+                operatorStack[operatorStackLength++] = b[i];
+            } else if (b[i] == "(") {
+                operatorStack[operatorStackLength++] = b[i];
+            } else if (b[i] == ")") {
+                while (operatorStackLength > 0 && operatorStack[operatorStackLength - 1] != "(") {
+                    output[outputLength++] = string(abi.encodePacked(operatorStack[--operatorStackLength]));
+                }
+                operatorStackLength--;
+            }
         }
-
+        
+        while (operatorStackLength > 0) {
+            output[outputLength++] = string(abi.encodePacked(operatorStack[--operatorStackLength]));
+        }
+        
+        return slice(output, 0, outputLength);
+    }
+    
+    function isNumber(string memory s) internal pure returns (bool) {
+        bytes memory b = bytes(s);
+        return b.length > 0 && b[0] >= "0" && b[0] <= "9";
+    }
+    
+    function parseInt(string memory s) internal pure returns (int256) {
+        bytes memory b = bytes(s);
+        int256 result = 0;
+        
+        for (uint256 i = 0; i < b.length; i++) {
+            result = result * 10 + int256(uint256(uint8(b[i])) - 48);
+        }
+        
         return result;
     }
-
-    function performOperation(uint256 x, uint256 y, uint256 operation) private pure returns (uint256) {
-        if (operation == 1) {
-            return x + y; // Addition
-        } else if (operation == 2) {
-            return x - y; // Subtraction
-        } else if (operation == 3) {
-            return x * y; // Multiplication
-        } else if (operation == 4) {
-            require(y != 0, "Division by zero"); // Division
-            return x / y;
-        }
-
-        revert("Invalid operation");
+    
+    function applyOperator(int256 a, bytes1 op, int256 b) internal pure returns (int256) {
+    if (op == "+") {
+        return a + b;
+    } else if (op == "-") {
+        return a - b;
+    } else if (op == "*") {
+        return a * b;
+    } else if (op == "/") {
+        return a / b;
+    } else {
+        revert("Invalid operator");
     }
+}
 
-    function findClosingParenthesis(bytes memory expr, uint256 startIndex) private pure returns (uint256) {
-        uint256 openParenthesisCount = 1;
-        uint256 i = startIndex;
-
-        while (i < expr.length) {
-            uint8 byteValue = uint8(expr[i]);
-
-            if (byteValue == 40) {
-                openParenthesisCount++;
-            } else if (byteValue == 41) {
-                openParenthesisCount--;
-            }
-
-            if (openParenthesisCount == 0) {
-                return i;
-            }
-
-            i++;
+    
+    function precedence(bytes1 op) internal pure returns (uint256) {
+        if (op == "+" || op == "-") {
+            return 1;
+        } else if (op == "*" || op == "/") {
+            return 2;
+        } else {
+            return 0;
         }
-
-        revert("Unbalanced parentheses");
     }
-
-    function substring(bytes memory expr, uint256 startIndex, uint256 endIndex) private pure returns (string memory) {
-        bytes memory result = new bytes(endIndex - startIndex + 1);
-
-        for (uint256 i = startIndex; i <= endIndex; i++) {
-            result[i - startIndex] = expr[i];
+    
+    function substring(string memory s, uint256 start, uint256 end) internal pure returns (string memory) {
+        bytes memory b = bytes(s);
+        bytes memory result = new bytes(end - start);
+        
+        for (uint256 i = start; i < end; i++) {
+            result[i - start] = b[i];
         }
-
+        
         return string(result);
+    }
+    
+    function slice(string[] memory arr, uint256 start, uint256 end) internal pure returns (string[] memory) {
+        string[] memory result = new string[](end - start);
+        
+        for (uint256 i = start; i < end; i++) {
+            result[i - start] = arr[i];
+        }
+        
+        return result;
     }
 }
